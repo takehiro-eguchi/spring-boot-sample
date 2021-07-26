dd1d4ab8cca2035a43c79d62e947b9fa0f748896
Takehiro Eguchi <t-eguchi@nssys.co.jp>
2021-07-26 14:49:32 +0900

    update

---
+++ spring-sample/src/test/java/com/egu/springboot/sample/SpringSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egu.springboot.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
